5cbb2e41bb32a3184d2663f678b705aef823dc85
Alexander Turenko <alexander.turenko@tarantool.org>
2020-09-11 04:07:12 +0300

    WIP: module api/lua: add luaT_tuple_encode()
    It is the same as luaT_tuple_new(), but returns raw MsgPack data (not
    <box_tuple_t>) allocated on Lua shared ibuf (<tarantool_lua_ibuf>).
    
    The reason to introduce this function is to provide ability to use
    box_tuple_compare_with_key() function from an external module for a key
    passed as a Lua table. The compare function has the following signature:
    
     | API_EXPORT int
     | box_tuple_compare_with_key(box_tuple_t *tuple_a, const char *key_b,
     |                            box_key_def_t *key_def);
    
    The second parameter is a key encoded as an MsgPack array, not a tuple
    structure. So luaT_tuple_new() is not applicable here (it is not
    worthful to create a tuple structure if we need just MsgPack data).
    
    XXX: Add a module API test.
    
    Part of #5273

--- src/box/lua/tuple.c
+++ src/box/lua/tuple.c
@@ -98,8 +98,8 @@ luaT_istuple(struct lua_State *L, int narg)
 	return *(struct tuple **) data;
 }
 
-box_tuple_t *
-luaT_tuple_new(struct lua_State *L, int idx, box_tuple_format_t *format)
+char *
+luaT_tuple_encode(struct lua_State *L, int idx, size_t *tuple_len_ptr)
 {
 	if (idx != 0 && !lua_istable(L, idx) && !luaT_istuple(L, idx)) {
 		diag_set(IllegalParams, "A tuple or a table expected, got %s",
@@ -127,8 +127,21 @@ luaT_tuple_new(struct lua_State *L, int idx, box_tuple_format_t *format)
 		luamp_encode_tuple(L, &tuple_serializer, &stream, idx);
 	}
 	mpstream_flush(&stream);
-	box_tuple_t *tuple = box_tuple_new(format, buf->buf,
-					   buf->buf + ibuf_used(buf));
+	if (tuple_len_ptr != NULL)
+		*tuple_len_ptr = ibuf_used(buf);
+	return buf->buf;
+
+}
+
+box_tuple_t *
+luaT_tuple_new(struct lua_State *L, int idx, box_tuple_format_t *format)
+{
+	size_t tuple_len;
+	char *tuple_data = luaT_tuple_encode(L, idx, &tuple_len);
+	if (tuple_data == NULL)
+		return NULL;
+	box_tuple_t *tuple = box_tuple_new(format, tuple_data,
+					   tuple_data + tuple_len);
 	if (tuple == NULL)
 		return NULL;
 	ibuf_reinit(tarantool_lua_ibuf);
